2f0e428fb9ad63983990f228ec84003fda97b689
cyb2rZ <alisheroromov185@gmail.com>
2024-04-22 12:08:58 +0500

    I finished header section

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+const navMenu = document.querySelector('nav')
+const bars = document.querySelector('.menu-bars')
+
+function showMenu() {
+
+}
+bars.addEventListener('click', (e) => {
+  navMenu.classList.toggle('responsive')
+
+})
